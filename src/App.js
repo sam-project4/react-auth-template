1b6a27c464340d41ec1eedda18010cd0cee75a60
Abdullah <ab4ullahkh@gmail.com>
2019-03-28 12:53:33 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import Nav from "./components/Nav";
+import "./App.css";
+import { getUser, Signout } from "./services/AuthService";
+import SigninForm from "./components/authForm.js/SigninForm";
+import SignupForm from "./components/authForm.js/SignupForm";
+import ChangePasswordForm from "./components/authForm.js/ChangePasswordForm";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+class App extends Component {
+  state = {
+    user: null,
+    activePage: "home"
+  };
+  componentDidMount() {
+    // check if we have a token in the local storage
+    const user = getUser();
+    if (user) {
+      this.setState({ user });
+    }
+  }
+
+  changeActivePage = activePage => {
+    this.setState({ activePage });
+  };
+  onSignin = () => {
+    this.setState({ user: getUser() });
+    this.changeActivePage("profile");
+  };
+  onSignout = () => {
+    console.log("sigin out");
+    this.setState({ user: null });
+    Signout();
+  };
+  render() {
+    const { user, activePage } = this.state;
+    return (
+      <div>
+        <Nav
+          user={user}
+          changeActivePage={this.changeActivePage}
+          onSignout={this.onSignout}
+        />
+
+        <div className="container">
+          {activePage === "home" ? <Home /> : ""}
+          {activePage === "sign-in" ? (
+            <SigninForm onSignin={this.onSignin} />
+          ) : (
+            ""
+          )}
+          {activePage === "sign-up" ? (
+            <SignupForm onSignin={this.onSignin} />
+          ) : (
+            ""
+          )}
+          {activePage === "change-password" ? (
+            <ChangePasswordForm changeActivePage={this.changeActivePage} />
+          ) : (
+            ""
+          )}
+          {activePage === "profile" ? <Profile /> : ""}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/services/AuthService.js
@@ -0,0 +1,18 @@
+export function setUser(user) {
+  localStorage.setItem("user", JSON.stringify(user));
+}
+
+const delete_cookie = function(name) {
+  document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+};
+
+export function getUser() {
+  const user = localStorage.getItem("user");
+  return user === "undefined" ? null : JSON.parse(user);
+}
+
+export function Signout() {
+  document.cookie = "jwt= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+  delete_cookie("jwt");
+  localStorage.removeItem("user");
+}
