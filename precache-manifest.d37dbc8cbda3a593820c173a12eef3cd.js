eee7aaf9a713418cf8f2f94fb5866cc51e0e1aa1
Abdullah <ab4ullahkh@gmail.com>
2019-04-04 09:29:55 +0300

    Updates

---
+++ precache-manifest.d37dbc8cbda3a593820c173a12eef3cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad9b0365218e82f06e8",
+    "url": "/react-auth-template/static/css/main.d8293b9b.chunk.css"
+  },
+  {
+    "revision": "fad9b0365218e82f06e8",
+    "url": "/react-auth-template/static/js/main.e60c9fbb.chunk.js"
+  },
+  {
+    "revision": "3345b56d7ed7a0ca6151",
+    "url": "/react-auth-template/static/js/runtime~main.b2e03bd4.js"
+  },
+  {
+    "revision": "eb7de36fac500d28aa29",
+    "url": "/react-auth-template/static/css/2.92ad7583.chunk.css"
+  },
+  {
+    "revision": "eb7de36fac500d28aa29",
+    "url": "/react-auth-template/static/js/2.4a6d4185.chunk.js"
+  },
+  {
+    "revision": "71ca648057233441c08f6f499bdc9c6a",
+    "url": "/react-auth-template/index.html"
+  }
+];
